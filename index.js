ed7e5a36bb6e24786d04590f17424342f3ce8d38
gasparg <gaspi-gonzalez@hotmail.com>
2022-03-28 00:01:12 -0300

    agregando js a registrar

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ class InicioSesion {
 
 //PEDIMOS DE LOCAL STORAGE LOS DATOS DE REGISTRO
 let valorDatos = localStorage.getItem('datos');
+console.log(valorDatos);
 
 
 //PASAMOS LOS DATOS DE STRING A OBJETO
@@ -24,7 +25,10 @@ const sesion = document.getElementById("inicio");
 
 //OBTENEMOS LOS DATOS DE LOS INPUTS
 let email = document.getElementById("email");
-let pass1 = document.getElementById("pass");
+let password = document.getElementById("pass");
+let boxMsj = document.getElementById("miModal");
+let parrafo = document.getElementById("parrafo");
+let titulo = document.getElementById("titulo");
 
 
 //EVENTO SUBMIT DEL FORMULARIO CON LA FUNCION VALIDAR LOS DATOS
@@ -36,17 +40,44 @@ function validacion(e){
     e.preventDefault();
 
     //CREAMOS UN NUEVO OBJETO INICIO SESION
-    let datoInicio = new InicioSesion(email.value, pass1.value);
+    let datoInicio = new InicioSesion(email.value, password.value);
+    
 
-    //COMPARAMOS SI SON IGUALES LOS DATOS
-    if(datoInicio.email == pasoParse.email && datoInicio.pass1 == pasoParse.pass1){
-        console.log("datos correctos");
-        location.href = "paginaprincipal.html";
 
-    }else{
-        console.log("Datos Incorrectos");
-        alert("Datos Incorrectos");
+    if(password.value == "" || email.value == ""){
+        boxMsj.style.display = "block";
+        titulo.innerText = "Error";
+        parrafo.innerText = "Datos Incorrectos";
+        email.classList.add('error1');
+
+        setTimeout(()=>{
+            boxMsj.style.display = "none";
+        },1000);
     }
+        else if(datoInicio.email != pasoParse.email){
+            boxMsj.style.display = "block";
+            titulo.innerText = "Error";
+            parrafo.innerText = "Email Incorrecto";
+            email.classList.add('error1');
+
+            setTimeout(()=>{
+                boxMsj.style.display = "none";
+            },1000);
+    }
+        else if(datoInicio.contraseña != pasoParse.contraseña){
+            boxMsj.style.display = "block";
+            titulo.innerText = "Error";
+            parrafo.innerText = "Contraseña Incorrecta";
+            email.classList.add('error1');
+
+            setTimeout(()=>{
+                boxMsj.style.display = "none";
+            },1000);
+        }
+        else if(datoInicio.email === pasoParse.email && datoInicio.contraseña === pasoParse.contraseña){
+            location.href = "paginaprincipal.html";
+        }
+
 }
 
 
--- registro.js
+++ registro.js
@@ -21,12 +21,11 @@ const registro = document.getElementById("formulario"); //obtenemos el formulari
     let nombre = document.getElementById("name");
     let modal = document.getElementById("miModal");
     let parrafo = document.getElementById("parrafo");
+    let titulo = document.getElementById("titulo");
+    let error = document.getElementsByClassName("form-control");
     
 
-    //funcion que inclute el redireccionamiento a index
-    function volverIndex (){
-        location.href = "index.html";
-    }
+
 
 
 //obtenemos el evento de enviar
@@ -35,10 +34,72 @@ registro.addEventListener("submit", envioFormulario); //llamamos a la funcion en
 function envioFormulario(e){
     e.preventDefault(); //frenamos la acrualizacion de la pagina
 
-    let datoNuevo = new Datos(nombre.value, email.value, pass1.value, );
+    //Le saco la clase error para que por default no tenga ninguna
+    window.addEventListener("click",()=>{
+        nombre.classList.remove('error');
+        mail.classList.remove('error');
+        pass1.classList.remove('error');
+    });
+
 
     
+    
+//Si todos los campos estan vacios da error de datos imcompletos
+if(nombre.value == "" && email.value =="" && pass1.value == ""){
+        titulo.innerText = "error";
+        parrafo.innerText = "Campos Incompletos";
+        modal.style.display = "block";
+        setTimeout(()=>{
+            modal.style.display = "none";
+        },1000);
 
+        mail.classList.add('error');
+        nombre.classList.add('error');
+        pass1.classList.add('error');
+        
+
+}   
+  //si el campo de nombre esta vacio
+    else if(nombre.value == ""){
+        titulo.innerText = "error";
+        parrafo.innerText = "Nombre Incompleto";
+        modal.style.display = "block";
+        setTimeout(()=>{
+            modal.style.display = "none";
+        },1000);
+
+        nombre.classList.add('error');
+}
+    //si el campo de email esta vacio
+    else if(mail.value == ""){
+        titulo.innerText = "error";
+        parrafo.innerText = "Email Incompleto";
+        modal.style.display = "block";
+        setTimeout(()=>{
+            modal.style.display = "none";
+        },1000);
+
+        mail.classList.add('error');
+}
+    //si el campo de contraseña esta vacio
+    else if(pass1.value == ""){
+        titulo.innerText = "error";
+        parrafo.innerText = "Contraseña Incompleta";
+        modal.style.display = "block";
+        //se espera tiempo para que se vea el mensaje 
+        setTimeout(()=>{
+            modal.style.display = "none";
+        },1000);
+
+        pass1.classList.add('error');
+}
+    
+else{
+
+    //si se ingresan todos los datos se crea el objeto se limpian los campos y se guardan los datos en el localstorage con un tiempo de espera asi se ve el mensaje
+
+    let datoNuevo = new Datos(nombre.value, email.value, pass1.value, );
+       
     //limpiar inputs
     nombre.value = "";
     email.value = "";
@@ -46,11 +107,15 @@ function envioFormulario(e){
 
 
     localStorage.setItem('datos',JSON.stringify(datoNuevo)); //tomo datoNuevo.email como el key para guardar en el local
-   
+    
+    titulo.innerText = "Registro Existoso!"
     parrafo.innerText = "Gracias " + datoNuevo.nombre ;
     modal.style.display = "block";
     
     //le pongo timeOut para que el mensaje se pueda leer unos dos segundos
-    setTimeout(volverIndex, 2000);
-}
+    setTimeout(()=>{
+        location.href = "index.html";
+    }, 1000);
+    }
 
+}
