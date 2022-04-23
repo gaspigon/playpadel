63650fb36fbd6ab2b8d4747b98ed99552ec7f60f
gasparg <gaspi-gonzalez@hotmail.com>
2022-04-22 22:03:03 -0300

    agregando torneos js|html|css

--- torneos.js
+++ torneos.js
@@ -1,16 +1,46 @@
 let busqueda = document.getElementById("search");
 
+//traigo el elemento div donde voy a crear lo capurado de el .json
+const box = document.getElementById("row");
+
 
 
 busqueda.addEventListener("keyup", (e)=>{
     
     let text = e.target.value;
-    let textMostrar = "";
-    
-        if(text !== ""){
-            textMostrar = text;
-        }
-    document.getElementById("mostrarText").innerHTML = textMostrar;
+     
+      
+//utilzo fetch para comunicarme con el .json   
+fetch('torneos.json')
+.then ((res) => res.json())
+.then( (data) => {
+
+   box.innerHTML = "";
+    //recorro el json y creo el elemento div para desp mostrarlo
+    //si el nivel de jugador y la posicion coinciden con la busqueda lo muestro
+    data.forEach((torneos) => {
+        const div = document.createElement('div');
+        div.classList.add("col-4");
+        div.classList.add("box-torneo");
+         if(text == torneos.nivel ){
+        div.innerHTML = `
+            <p class="p-torneo">${torneos.club}
+                <ul class="lista-tor">
+                    <li>Dia: ${torneos.dia}</li>
+                    <li>Horario: ${torneos.horario}</li>
+                    <li>Inscripcion: ${torneos.pago}</li>
+                    
+                </ul>
+
+                <button class="btn-contacto">Inscribirse</button>
         
+          `
+
+         box.append(div);
+         }
+
+})
+
+})
 
 });
