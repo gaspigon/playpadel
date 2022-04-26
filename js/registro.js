8575b9edabd555dd730271254fc4b0e528fafdcd
gasparg <gaspi-gonzalez@hotmail.com>
2022-04-25 22:54:16 -0300

    creado footer

---
+++ js/registro.js
@@ -0,0 +1,134 @@
+
+
+//clase de datos para el registro de personas
+class Datos {
+    constructor(nombre, email, contraseña){
+        this.nombre = nombre;
+        this.email = email;
+        this.contraseña = contraseña;
+
+    }
+}
+
+
+const registro = document.getElementById("formulario"); //obtenemos el formulario 
+
+
+//obtenemos los inputs del formulario registro
+
+    let mail = document.getElementById("email");
+    let pass1 = document.getElementById("pass");
+    let nombre = document.getElementById("name");
+    let modal = document.getElementById("miModal");
+    let incompleto = document.getElementsByClassName("incomplete");
+    let parrafo = document.getElementById("parrafo");
+    let titulo = document.getElementById("titulo");
+    let error = document.getElementsByClassName("form-control");
+    
+
+
+
+
+//obtenemos el evento de enviar
+registro.addEventListener("submit", envioFormulario); //llamamos a la funcion enviar formulario
+
+function envioFormulario(e){
+    e.preventDefault(); //frenamos la acrualizacion de la pagina
+
+    //Le saco la clase error para que por default no tenga ninguna
+    window.addEventListener("click",()=>{
+        nombre.classList.remove('error');
+        mail.classList.remove('error');
+        pass1.classList.remove('error');
+    });
+
+
+    
+    
+//Si todos los campos estan vacios da error de datos imcompletos
+if(nombre.value == "" && email.value =="" && pass1.value == ""){
+        for (let i = 0; i < incompleto.length; i++) {
+            incompleto[i].style.display = "block";
+        }
+        mail.classList.add('error');
+        nombre.classList.add('error');
+        pass1.classList.add('error');
+
+        nombre.addEventListener("keydown",()=>{
+            incompleto[0].style.display = "none";
+        });
+
+        
+        mail.addEventListener("keydown",()=>{
+            incompleto[1].style.display = "none";
+        });
+
+        
+        pass1.addEventListener("keydown",()=>{
+            incompleto[2].style.display = "none";
+        });
+
+        
+
+}   
+  //si el campo de nombre esta vacio
+    else if(nombre.value == ""){
+
+        incompleto[0].style.display = "block";
+        nombre.classList.add('error');
+
+        nombre.addEventListener("keydown",()=>{
+            incompleto[0].style.display = "none";
+        });
+}
+    //si el campo de email esta vacio
+    else if(mail.value == ""){
+        incompleto[1].style.display = "block";
+
+        mail.classList.add('error');
+
+        mail.addEventListener("keydown",()=>{
+            incompleto[1].style.display = "none";
+        });
+}
+    //si el campo de contraseña esta vacio
+    else if(pass1.value == ""){
+        incompleto[2].style.display = "block";
+        pass1.classList.add('error');
+
+        pass1.addEventListener("keydown",()=>{
+            incompleto[2].style.display = "none";
+        });
+}
+    
+else{
+
+    //si se ingresan todos los datos se crea el objeto se limpian los campos y se guardan los datos en el localstorage con un tiempo de espera asi se ve el mensaje
+
+    let datoNuevo = new Datos(nombre.value, email.value, pass1.value, );
+       
+    //limpiar inputs
+    nombre.value = "";
+    email.value = "";
+    pass1.value = "";
+
+
+    localStorage.setItem('datos',JSON.stringify(datoNuevo)); //tomo datoNuevo.email como el key para guardar en el local
+    
+  
+Swal.fire({
+  position: 'center',
+  width: 200,
+  icon: 'success',
+  title: 'Registro Exitoso',
+  showConfirmButton: false,
+  timer: 1500
+})
+    
+    //le pongo timeOut para que el mensaje se pueda leer unos dos segundos
+    setTimeout(()=>{
+        location.href = "index.html";
+    }, 1000);
+    }
+
+}
---
+++ js/reservas.js
@@ -0,0 +1,50 @@
+
+
+
+//clase de datos para el registro de personas
+class Reserva {
+    constructor(dia, hora){
+        this.dia = dia;
+        this.hora = hora;
+    }
+}
+
+const formulario = document.getElementById("formReserva");
+
+let dia = document.getElementById("date");
+let horario = document.getElementById("hora");
+let id = Math.floor(Math.random() * (100-0+1)) +1;
+
+
+formulario.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+
+    let reservaNueva = new Reserva (dia.value, horario.value);
+    
+    Toastify({
+
+        text: "Turno Reservado " + horario.value ,
+        gravity: "top", 
+        position: "right", 
+        duration: 1000,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+        
+        }).showToast();
+
+        localStorage.setItem(id, JSON.stringify(reservaNueva));
+        
+
+        console.log(reservaNueva);
+
+        //volvemos a la pagina principal
+        setTimeout(()=>{
+           location.href = "paginaprincipal.html";
+       },1200);
+
+})
+
+
+
